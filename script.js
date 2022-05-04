c58eeb7c852784267ebd7824f818b477c1d2cbbf
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-05-04 20:48:20 -0300

    Play sound

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("keydown", function (e) {
   const audio = document.querySelector(`audio[data-key="${e.key}"]`);
-  console.log(audio);
-  console.log(e.key);
+  if (!audio) return; //stop function from running
+  audio.play();
 });
