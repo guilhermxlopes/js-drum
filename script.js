cc522b785aabd779d39cfd72b596a675e0c37c15
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-05-04 20:49:19 -0300

    Repetition for keys

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("keydown", function (e) {
   const audio = document.querySelector(`audio[data-key="${e.key}"]`);
   if (!audio) return; //stop function from running
+  audio.currentTime = 0;
   audio.play();
 });
