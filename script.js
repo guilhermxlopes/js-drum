ea11e562fabf0efa5d5563908a91b3d9f84c814f
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-05-04 21:19:21 -0300

    JS remove style

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 document.addEventListener("keydown", function (e) {
   const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
+
   if (!audio) return; //stop function from running
+
   audio.currentTime = 0;
   audio.play();
+
+  key.classList.add("playing");
 });
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return; //skip it if it's not a transform
+  this.classList.remove("playing");
+
+  const keys = document.querySelectorAll(".key");
+  keys.forEach((key) =>
+    key.addEventListener("transitionend", removeTransition)
+  );
+}
